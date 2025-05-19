1aeacf22bb06cbe9e80ff40ca0444d4b534053a9
luizalvesrgithub <luizalvesr@gmail.com>
2025-05-19 13:48:39 -0300

    segundo commit

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Luiz Alves</h1>"))
+		w.Write([]byte("<h1>Hello Luiz Alves Rodrigues, a perseverança é a melhor amiga da aprendizagem</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
